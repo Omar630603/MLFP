9b2ee2fb4ff3c6276b9ab3026224ff461b3362a0
Omar630603 <omar.yem1111@gmail.com>
2021-06-28 15:25:36 +0700

    FIX

--- imgrec_webapp.py
+++ imgrec_webapp.py
@@ -8,19 +8,19 @@ app.run(debug=True)
 
 from keras.models import load_model 
 from keras.backend import set_session
+from keras.layers import LayerNormalization
 from skimage.transform import resize 
 import matplotlib.pyplot as plt 
-import tensorflow as tf 
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior() 
 import numpy as np 
 
 print("Loading model") 
 global sess
-sess = tf.compat.v1.Session()
-set_session(sess)
 global model 
 model = load_model('my_cifar10_model.h5') 
 global graph
-graph = tf.compat.v1.get_default_graph()
+graph = tf.get_default_graph()
 
 @app.route('/', methods=['GET', 'POST']) 
 def main_page():
@@ -40,7 +40,9 @@ def prediction(filename):
     
     #Step 3
     with graph.as_default():
+      sess = tf.Session() 
       set_session(sess)
+      model = load_model('my_cifar10_model.h5')
       probabilities = model.predict(np.array( [my_image_re,] ))[0,:]
       print(probabilities)
       #Step 4
